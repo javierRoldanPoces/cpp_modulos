d2e4df6f6bc2483bacfb64e524915531852e8354
Jroldan- <javierroldanpoces@gmail.com>
2024-04-20 20:53:14 +0200

    cpp02/ex01

--- cpp_02/ex01/Fixed.cpp
+++ cpp_02/ex01/Fixed.cpp
@@ -5,12 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: javier <javier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/20 15:56:20 by javier            #+#    #+#             */
-/*   Updated: 2024/04/20 18:07:00 by javier           ###   ########.fr       */
+/*   Created: 2024/04/20 18:08:46 by javier            #+#    #+#             */
+/*   Updated: 2024/04/20 20:51:15 by javier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Fixed.hpp"
+#include <cmath>
+
 
 // Constructor por defecto.
 Fixed::Fixed()
@@ -18,6 +21,27 @@ Fixed::Fixed()
     std::cout << "Default constructor called" << std::endl;
     this->_fixed_point = 0;
 }
+
+/*
+Sobrecarga Constructor recibe int.
+
+A constructor that takes a constant integer as a parameter.
+It converts it to the corresponding fixed-point value. The fractional bits value is
+initialized to 8 like in exercise 00
+*/
+Fixed::Fixed(const int integ)
+{
+	std::cout << "Int constructor called" << std::endl;
+	_fixed_point = integ << _fractional_bits; // Equivale a multipilicar integ * 2⁸.Pilla un int y lo convierte a punto flotante.
+}
+
+//Sobrecarga constructor recibe float.
+Fixed::Fixed(const float floating_point)
+{
+	std::cout << "Float constructor called" << std::endl;
+	_fixed_point = roundf(floating_point * (1 << _fractional_bits));//Pilla un float y lo convierte a punto flotante.
+}
+
 
 // Constructor de copia.
 Fixed::Fixed(const Fixed& name)
@@ -31,7 +55,7 @@ const Fixed&	Fixed::operator =(const Fixed& name)
 {
     std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &name) // Para evitar que se haga una reasignacion.
-		_fixed_point = name.getRawBits();  //name._fixed_point;	
+		_fixed_point = name._fixed_point; //name.getRawBits();  // (Modificación para output = que en subject)
 	return (*this);
 }
 
@@ -41,16 +65,36 @@ Fixed::~Fixed()
 	std::cout << "Destructor called" << std::endl;
 }
 
-// Función miembro que devuelve el valor de _fixed_point
+// Función miembro que devuelve el valor de _fixed_point.
 int Fixed::getRawBits(void) const
 {
 	std::cout << "getRawBits member function called" << std::endl;
 	return (this->_fixed_point);
 }
 
-// Funcion miembro para asignar un valor a _fixed_point
+// Funcion miembro para asignar un valor a _fixed_point.
 void Fixed::setRawBits(int const raw)
 {
 	std::cout << "setRawBits member function called" << std::endl;
 	this->_fixed_point =raw;
+}
+
+//Funcion miembro que convierte el _fixed_point a float.
+float   Fixed::toFloat(void) const
+{
+	float	res;
+	res = ((float)_fixed_point / (1 << _fractional_bits));
+	return (res);
+}
+
+//Funcion miembro que convierte el _fixed_point a int.
+int	Fixed::toInt(void) const
+{
+	return (_fixed_point >> _fractional_bits);
+}
+
+std::ostream& operator<<(std::ostream& outstream, const Fixed& name)
+{
+    outstream << name.toFloat();
+    return outstream;
 }
