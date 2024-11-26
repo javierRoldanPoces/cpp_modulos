def528c069e002ab62299c6336876f81c1a52449
Javi <javierroldanpoces@gmail.com>
2024-11-26 20:50:35 +0100

    cpp05/ex01 en progreso

---
+++ cpp_05/ex01/Bureaucrat.cpp
@@ -0,0 +1,103 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat():_name("default"), _grade(150)
+{
+
+}
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade)
+:_name(name), _grade(grade)
+{
+    try
+    {
+        if (grade < 1)
+		{
+          	_grade = 1;
+			throw Bureaucrat::GradeTooHighException();
+
+		}
+        else if (grade > 150)
+		{
+			_grade = 150;
+            throw Bureaucrat::GradeTooHighException();
+		}
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << grade << std::endl;
+    }
+    
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy)
+:_name(copy._name), _grade(copy._grade)
+{
+
+}
+
+Bureaucrat&	Bureaucrat::operator=(const Bureaucrat &copy)
+{
+	_grade = copy._grade;
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+
+}
+
+const std::string	Bureaucrat::getName()const
+{
+	return this->_name;
+}
+
+int	Bureaucrat::getGrade()const
+{
+	return this->_grade;
+}
+
+void Bureaucrat::incrementGrade()
+{
+	try
+	{
+		if (this->_grade <= 1)
+			throw Bureaucrat::GradeTooHighException();
+		else
+			this->_grade--;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << this->getGrade() << std::endl;
+	}
+}
+
+void Bureaucrat::decrementGrade()
+{
+	try
+	{
+		if (this->_grade >= 150)
+			throw Bureaucrat::GradeTooLowException();
+		else
+			this->_grade++;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << this->getGrade() << std::endl;
+	}
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return "error: Grade too high ";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return "error: Grade too low ";
+}
+
+std::ostream& operator<<(std::ostream& st, const Bureaucrat b)
+{
+	st <<b.getName()  << ", bureaucrat grade " << b.getGrade() << std::endl;
+	return st;
+}
---
+++ cpp_05/ex01/Form.hpp
@@ -0,0 +1,65 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+#include "Bureaucrat.hpp"
+#include <iostream>
+#include <exception>
+
+class   Form
+ {
+    private:
+        const	std::string _name;
+        bool	isSigned;
+        const	int _gradeSign;
+		const	int _gradeExecute;
+    
+	public:
+		Form();
+		Form(const std::string &name, bool isSigned, int gSign, int gExec);
+		Form(const Form &copy);
+		Form& operator=(const Form &copy);
+
+		~Form();
+
+		//getter
+		const 	std::string name getName() const;
+		bool	get_isSigned() const;
+		int		get_gradeSign() const;
+		int		get_gradeExecute() const;
+
+		beSigned(const Bureaucrat &b); //Remember, grade 1 is higher than grade 2.
+										//If the grade is too low, throw a Form::GradeTooLowException.
+		/*
+		The grades of the Form follow the same rules that apply to the Bureaucrat. Thus,
+		the following exceptions will be thrown if a form grade is out of bounds:
+		Form::GradeTooHighException and Form::GradeTooLowException.
+		*/
+		class	GradeTooHighException: pubic std::exception
+		{
+			public:
+				const char *what()const throw();
+		};
+
+		class	GradeTooLowException: public std::exception
+		{
+			public:
+				const char *what()const throw();
+		};
+
+ };
+ //overload of insertion
+
+ std::ostream& operator<<(std::ostream& st, const Form& form);
+	
+
+ #endif
+ /*
+ OJO FALTA
+
+ Lastly, add a signForm() member function to the Bureaucrat. If the form got signed,
+it will print something like:
+<bureaucrat> signed <form>
+Otherwise, it will print something like:
+<bureaucrat> couldn’t sign <form> because <reason>.
+Implement and turn in some tests to ensure everything works as expected.
+ 
+ */
